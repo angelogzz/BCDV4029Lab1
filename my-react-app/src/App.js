d4ef79c920ec7a7a2895cd31b20df098ae92f16b
angelogzz <angelogzz87@gmail.com>
2023-10-03 22:31:48 -0400

    App and Greeter

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Gretter from "./Greeter"; //Import the Gretter component
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my React app</h1>
+      <Gretter /> {/*Use the Gretter component*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Greeter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gretter() {
+  return <h2>Hello React!</h2>;
+}
+
+export default Gretter;
